3c9c4879c0a79a46d97a776b2fe14ddbd90f055f
Vinay Vora <voravinay82@gmail.com>
2025-02-26 21:30:03 +0530

    final please3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,63 +1,39 @@
-import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
   publicRoutes: [
-    "/",
-    "/sign-in",
-    "/sign-up",
-    "/api/clerk-webhook",
-    "/api/drive-activity/notification",
-    "/api/payment/success",
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
+    '/',
+    '/api/clerk-webhook',
+    '/api/drive-activity/notification',
+    '/api/payment/success',
+    "/pricing",
+    "/products",
+    "/clients",
+    "/resources",
+    "/documentation",
+    "/enterprise",
+    "/privacy",
+    "/terms",
+    '/api/cron',
+    '/api/create-user',  // Add this route if it should be publicly accessible
+    '/api/update-user'   // Add this route if it should be publicly accessible
   ],
-
   ignoredRoutes: [
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
-    "/api/auth/callback/discord",
-    "/api/auth/callback/notion",
-    "/api/auth/callback/slack",
-    "/api/flow",
-    "/api/cron/wait",
+    '/api/auth/callback/discord',
+    '/api/auth/callback/notion',
+    '/api/auth/callback/slack',
+    '/api/flow',
+    '/api/cron/wait',
+    '/api/cron',
   ],
-
-  afterAuth(auth, req) {
-    // Check if the current route is in ignoredRoutes
-    const isIgnoredRoute = ignoredRoutes.some(route => 
-      req.nextUrl.pathname.startsWith(route)
-    );
-
-    // Handle requests to ignored routes
-    if (isIgnoredRoute) {
-      return NextResponse.next();
-    }
-
-    // Redirect if not authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      const signInUrl = new URL('/sign-in', req.url);
-      // Preserve the current URL as a redirect after sign-in
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
-
-    return NextResponse.next();
-  },
-});
+})
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     * - public files with extensions (.svg, .jpg, etc)
-     */
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-  ],
-};
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}
+
+// https://www.googleapis.com/auth/userinfo.email
+// https://www.googleapis.com/auth/userinfo.profile
+// https://www.googleapis.com/auth/drive.activity.readonly
+// https://www.googleapis.com/auth/drive.metadata
+// https://www.googleapis.com/auth/drive.readonly
