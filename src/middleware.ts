475998cab78723cba044318422598a55ac514d35
Vinay Vora <voravinay82@gmail.com>
2025-02-26 20:39:52 +0530

    fixed for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,38 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
+  // Routes that can be accessed while signed out
   publicRoutes: [
-    '/',
-    '/api/clerk-webhook',
-    '/api/drive-activity/notification',
-    '/api/payment/success',
+    "/",
+    "/sign-in*",
+    "/sign-up*",
+    "/api/clerk-webhook",
+    "/api/drive-activity/notification",
+    "/api/payment/success",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
   ],
+
+  // Routes that can always be accessed, and have no authentication information
   ignoredRoutes: [
-    '/api/auth/callback/discord',
-    '/api/auth/callback/notion',
-    '/api/auth/callback/slack',
-    '/api/flow',
-    '/api/cron/wait',
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/api/auth/callback/discord",
+    "/api/auth/callback/notion",
+    "/api/auth/callback/slack",
+    "/api/flow",
+    "/api/cron/wait",
   ],
-})
+
+  afterAuth(auth, req) {
+    // Redirect users who aren't authenticated unless they are accessing a public route
+    if (!auth.userId && !auth.isPublicRoute) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  },
+});
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
-
-// https://www.googleapis.com/auth/userinfo.email
-// https://www.googleapis.com/auth/userinfo.profile
-// https://www.googleapis.com/auth/drive.activity.readonly
-// https://www.googleapis.com/auth/drive.metadata
-// https://www.googleapis.com/auth/drive.readonly
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
